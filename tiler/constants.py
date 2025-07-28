82574cf22fd22048057391a0217ded49c1ebc95d
Gauthier Glückmann <glugau@hotmail.be>
2025-07-28 15:59:35 +0200

    Changed colormaps

--- tiler/constants.py
+++ tiler/constants.py
@@ -2,8 +2,15 @@
 # together.
 
 CMAP_MAPPINGS = {
-    'temperature': 'inferno',
-    '2m_temperature': 'inferno',
+    'temperature': 'plasma',
+    '2m_temperature': 'plasma',
+    'sea_surface_temperature': 'plasma',
+    '10m_u_component_of_wind': 'bwr',
+    '10m_v_component_of_wind': 'bwr',
+    'u_component_of_wind': 'bwr',
+    'v_component_of_wind': 'bwr',
+    'total_precipitation': 'YlGnBu',
+    'specific_humidity': 'YlGnBu'
 }
 
 CMAP_DEFAULT = 'viridis'
