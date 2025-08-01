851a3fae4dc54281373b9e5c2e40d640b433824b
Gauthier Glückmann <glugau@hotmail.be>
2025-08-01 20:42:14 +0200

    Documentation

--- visualizer/js/config.js
+++ visualizer/js/config.js
@@ -1,6 +1,18 @@
 export const CONFIG = {
+    // Data URL containing the 'tiles' folder
     DATA_URL: 'https://data.appa-forecasts.download/',
-    LOOK_AHEAD_LAYERS: 4,
+
+    // When at time t, invisible layers will be loaded up to t +
+    // LOOK_AHEAD_LAYERS so that playback is smooth. Increasing this can cause
+    // performance issues (zooming or panning requires loading all of those
+    // layers for the new viewport), and decreasing it may worsen the live
+    // playback experience.
+    LOOK_AHEAD_LAYERS: 4, 
+
+    // Opacity of the weather data layer that is displayed above the continental
+    // map data
     LAYER_OPACITY: 0.8,
+    
+    // Maximum available zoom value in the tiles sourced from DATA_URL.
     MAX_AVAILABLE_ZOOM: 2
 };
